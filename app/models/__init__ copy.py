fedbb4c993c100c91a2ac66cb12b4b76dc2828bd
surnan <suresh_nandlall@outlook.com>
2024-09-03 20:20:50 -0400

    updated model files but gave them all "copy" suffix to avoid git conflicts

---
+++ app/models/__init__ copy.py
@@ -0,0 +1,12 @@
+from .db import db
+from .user import User
+from .db import environment, SCHEMA
+
+from .cartproduct import CartProduct
+from .favorite import Favorite
+from .order import Order
+from .orderproduct import OrderProduct
+from .product import Product
+from .productimage import ProductImage
+from .review import Review
+from .shoppingcart import ShoppingCart
---
+++ app/models/favorite copy.py
@@ -0,0 +1,25 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    productId = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Relationship
+    user = db.relationship('User', back_populates='favorites')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'productId': self.productId,
+            'created_at': self.created_at
+        }
