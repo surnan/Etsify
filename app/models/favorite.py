13092eb0441c9f39874ae6ca64daac698b55da59
Javier <javierLhernandez@yahoo.com>
2024-09-04 19:48:56 -0400

    seeds added: favorites, products, order_products, cart_products

---
+++ app/models/favorite.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+
+    # Do I have to set up a relationship?
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'product_id': self.product_id
+        }
