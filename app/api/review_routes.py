5a567afd1297385b293f7ce19f0f9581cfe7439d
micLira828 <micgep1015@gmail.com>
2024-09-08 14:42:46 -0700

    Fixed error with quotes in request data for post of reviews

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -12,33 +12,27 @@ def get_all_reviews():
 @review_routes.route('/', methods=['POST'])
 def add_review():
     data = request.get_json()
-
     stars = data.get("stars")
     review = data.get("review")
     userId = data.get("userId")
     productId = data.get("productId")
     new_review = Review(stars=stars, review=review, userId=userId, productId=productId)
-
-    return render_template('review_page.html', all_reviews=[new_review])
-
-    print("=== NEW_REVIEW ======")
-    print(new_review)
-    print("=========")
-    print("=========")
-
-
-
-
+    print('new_review ', new_review)
+    print('new_review id ', new_review.id)
+    print('new_review stars ', new_review.stars)
+    print('new_review userId ', new_review.userId)
+    print('new_review productId', new_review.productId)
 
     try:
         db.session.add(new_review)
-        db.commit()
+        db.session.commit()
         return jsonify({"message": "Review added successfully", "review": {
             "stars": new_review.stars,
             "review": new_review.review,
             "userId": new_review.userId,
             "productId": new_review.productId
         }})
+        # return render_template('review_page.html', all_reviews=[new_review])
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 500
