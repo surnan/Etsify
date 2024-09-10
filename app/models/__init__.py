1cb9592d28d8ad9342a3b191ec2df5a307d5c6b3
surnan <suresh_nandlall@outlook.com>
2024-09-09 21:24:46 -0400

    Only Users Model File is operational.  All other models commented out

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,9 +2,9 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 
-from .cartproduct import CartProduct
-from .favorite import Favorite
-from .product import Product
-from .productimage import ProductImage
-from .review import Review
-from .shoppingcart import ShoppingCart
+# from .cartproduct import CartProduct
+# from .favorite import Favorite
+# from .product import Product
+# from .productimage import ProductImage
+# from .review import Review
+# from .shoppingcart import ShoppingCart
