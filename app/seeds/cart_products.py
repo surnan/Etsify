9cfaa6537f9ede1ac84a88a1db8e8a495afbcb14
surnan <suresh_nandlall@outlook.com>
2024-09-11 01:39:47 -0400

    Comment out cart_products seeder data

--- app/seeds/cart_products.py
+++ app/seeds/cart_products.py
@@ -2,23 +2,24 @@ from app.models import db, CartProduct, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_cart_products():
-    cart_product1 = CartProduct(
-        shoppingCartId=1, 
-        productId=1
-    )
-    cart_product2 = CartProduct(
-        shoppingCartId=1, 
-        productId=2
-    )
-    cart_product3 = CartProduct(
-        shoppingCartId=2, 
-        productId=3
-    )
+    a = 5
+    # cart_product1 = CartProduct(
+    #     shoppingCartId=1, 
+    #     productId=1
+    # )
+    # cart_product2 = CartProduct(
+    #     shoppingCartId=1, 
+    #     productId=2
+    # )
+    # cart_product3 = CartProduct(
+    #     shoppingCartId=2, 
+    #     productId=3
+    # )
 
-    db.session.add(cart_product1)
-    db.session.add(cart_product2)
-    db.session.add(cart_product3)
-    db.session.commit()
+    # db.session.add(cart_product1)
+    # db.session.add(cart_product2)
+    # db.session.add(cart_product3)
+    # db.session.commit()
 
 # def undo_cart_products():
 #     if environment == "production":
