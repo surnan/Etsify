4ade305bd20b2f8636509b577dc6b2356fe91826
surnan <suresh_nandlall@outlook.com>
2024-09-11 22:10:48 -0400

    Increasing length of Products.description

--- migrations/versions/20240905_192303_.py
+++ migrations/versions/20240905_192303_.py
@@ -36,7 +36,7 @@ def upgrade():
     op.create_table('products',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('sellerId', sa.Integer(), nullable=False),
-    sa.Column('name', sa.String(length=255), nullable=False),
+    sa.Column('name', sa.String(length=500), nullable=False),
     sa.Column('description', sa.String(length=500), nullable=True),
     sa.Column('price', sa.Float(), nullable=False),
     sa.Column('stock', sa.Integer(), nullable=False),
