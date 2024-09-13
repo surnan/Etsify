1ea446399f5825b3033c5c41fc7abe882f1d557a
micLira828 <micgep1015@gmail.com>
2024-09-13 09:27:04 -0700

    I started the component to delete a review

--- react-vite/src/components/Reviews/DeleteReview.jsx
+++ react-vite/src/components/Reviews/DeleteReview.jsx
@@ -1,7 +1,15 @@
-
-const DeleteReview = () => ({
-   
-    
-});
+// import {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import { useParams } from 'react-router-dom';
+import { deleteReviewThunk } from '../../redux/review';
+const DeleteReview = () =>{
+    let {reviewId} = useParams();
+    let review = useSelector((state) => state.reviews.byId[reviewId]);
+    const dispatch = useDispatch();
+    dispatch(deleteReviewThunk(reviewId));
+    return<>
+    <h2>Review Deleted</h2>
+    </>
+}
 
 export default DeleteReview;
